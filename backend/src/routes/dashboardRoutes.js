6deb049dd69dc1f2550475efa063616e61324bfb
Manuel Gutierrez Recio <162916542+Manuu058@users.noreply.github.com>
2026-04-15 17:07:34 +0200

    Cambios

--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDashboard } = require("../controllers/dashboardController");
-const { verificarToken } = require("../middlewares/authMiddleware");
-
-router.get("/", verificarToken, getDashboard);
-
-module.exports = router;
